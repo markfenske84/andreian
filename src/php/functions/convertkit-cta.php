a22bf6cbf0a779458b0ed10857e81ccf0b991cdb
Mark Fenske <mark.fenske@me.com>
2026-09-18 16:18:23 -0700

    dub h1 issue

--- src/php/functions/convertkit-cta.php
+++ src/php/functions/convertkit-cta.php
@@ -8,6 +8,31 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 /**
+ * Keep ConvertKit QuickTags (and its empty <h1> script template) off public pages.
+ *
+ * Kit loads editor modal markup on wp_enqueue_scripts for frontend editors.
+ * Naive crawlers count that empty heading. Visitors never need the modal.
+ */
+function andreian_disable_convertkit_frontend_quicktags() {
+	if ( is_admin() || ( is_user_logged_in() && current_user_can( 'edit_posts' ) ) ) {
+		return;
+	}
+
+	if ( ! function_exists( 'WP_ConvertKit' ) || ! class_exists( 'ConvertKit_Admin_TinyMCE' ) ) {
+		return;
+	}
+
+	$tinymce = WP_ConvertKit()->get_class( 'admin_tinymce' );
+
+	if ( ! $tinymce instanceof ConvertKit_Admin_TinyMCE ) {
+		return;
+	}
+
+	remove_action( 'wp_enqueue_scripts', array( $tinymce, 'register_quicktags' ) );
+}
+add_action( 'init', 'andreian_disable_convertkit_frontend_quicktags', 20 );
+
+/**
  * Render the site newsletter ConvertKit form.
  *
  * @param string $wrapper_class Wrapper class for layout styling.
